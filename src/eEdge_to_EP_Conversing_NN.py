4dce3b8b2886ca3736ff8af5e156d0b744ae879a
TecXCTO <cto.tecx@gmail.com>
2026-01-21 16:08:32 +0530

    Update eEdge_to_EP_Conversing_NN.py

--- src/eEdge_to_EP_Conversing_NN.py
+++ src/eEdge_to_EP_Conversing_NN.py
@@ -1,1 +1,12 @@
 
+# Elementary Edge to Elementary Particle Conversing Neural Network
+class EEtoEPCNN(self, parameters, *args, **kwargs):
+  def __init__(self):
+    #
+
+  def train(self):
+    return
+  def test(self):
+    return
+  def evaluate(self):
+    return
