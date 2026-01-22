b537bdc5958657a6c8f41a1cb300e8612b71d1dc
TecXCTO <cto.tecx@gmail.com>
2026-01-22 07:25:16 +0530

    Fix inheritance in EEtoEPCNN class

--- src/eEdge_to_EP_Conversing_NN.py
+++ src/eEdge_to_EP_Conversing_NN.py
@@ -1,6 +1,7 @@
-
+import numpy as np
+import EEtoEPCN
 # Elementary Edge to Elementary Particle Conversing Neural Network
-class EEtoEPCNN(EEtoEPCNN):
+class EEtoEPCNN(EEtoEPCN):
   def __init__(self, parameters, *args, **kwargs ):
     # self.param = parameters
   def train(self):
